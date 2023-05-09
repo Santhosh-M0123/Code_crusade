7bc6711e68206f833cd4ed07747e9d24ae272256
Santhosh <santhoshmsanthosh.1916@gmail.com>
2023-05-09 15:04:03 +0530

    from santhosh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import Home from './Home/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Home />
 );
 
